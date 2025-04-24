b8262d2c8f95547b7eadfebbba847c19b8c8623a
Jakob <minenash@protonmail.com>
2025-04-24 04:22:03 -0400

    Update metadata

--- common/src/main/java/com/minenash/command_waypoints/CommandWaypoints.java
+++ common/src/main/java/com/minenash/command_waypoints/CommandWaypoints.java
@@ -1,6 +1,5 @@
 package com.minenash.command_waypoints;
 
-import dev.architectury.injectables.annotations.PlatformOnly;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 
